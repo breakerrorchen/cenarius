b32ba3ae09cabb44b938050830214ca56dcca48f
chenguodong <chenguodong@bytedance.com>
2020-05-17 21:12:21 +0800

    feat: add webchat image

--- kernal_src/android_copy.sh
+++ kernal_src/android_copy.sh
@@ -1,2 +1,1 @@
-cp /Users/bytedance/my-github/cenarius/kernal_src/build/android/armv7-a/release/libcenarius.so \
-/Users/bytedance/Proj/Cenarius/bootloader/android/app/libs/armeabi-v7a
+cp ./build/android/armv7-a/release/libcenarius.so ../bootloader/android/app/libs/armeabi-v7a
