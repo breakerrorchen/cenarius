423489ed504c1fa7da5a43715dbff0c1d96d7c85
chenguodong <chenguodong@bytedance.com>
2020-05-27 17:01:06 +0800

    feat: add es 3.0 api wrapper

--- kernal_src/infrastructure/renderer/backend/es_render/_/_uniform_matrix_4x3_f_v.cc
+++ kernal_src/infrastructure/renderer/backend/es_render/_/_uniform_matrix_4x3_f_v.cc
@@ -6,5 +6,21 @@ using namespace renderer;
 
 void render_connector::uniform_matrix_4x3_f_v(
     int32_t index, uint32_t count, bool transpose, float* v) {
-    ::glUniformMatrix4x3fv((GLint)index, (GLsizei)count, (GLboolean)transpose, v);
+    if (nullptr == v || 0 == count) return;
+    auto ele_count = count * 4 * 3;
+    if (ele_count > 32) {
+        std::vector<GLfloat> _0; _0.resize(ele_count);
+        for (int i = 0; i < ele_count; ++i) {
+            _0[i] = (GLfloat)v[i];
+        }
+        ::glUniformMatrix4x3fv((GLint)index, 
+            (GLsizei)count, (GLboolean)transpose, &(_0[0])); 
+    } else {
+        GLfloat _0[32];
+        for (int i = 0; i < count; ++i) {
+            _0[i] = (GLfloat)v[i];
+        }
+        ::glUniformMatrix4x3fv((GLint)index, 
+            (GLsizei)count, (GLboolean)transpose, &(_0[0])); 
+    }
 }
